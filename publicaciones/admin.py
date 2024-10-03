dcb2b4f3f804d3a2488f9e3f9d4f122bc562d0c6
OsvaGFX <alu.20130845@correo.itlalaguna.edu.mx>
2024-10-03 12:10:12 -0600

    commit inicial

---
+++ publicaciones/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Publicacion
+
+# Create your models here.
+admin.site.register(Publicacion)
---
+++ publicaciones/urls.py
@@ -0,0 +1,7 @@
+# publicaciones/urls.py
+from django.urls import path
+from .views import VistaPaginaInicio
+
+urlpatterns = [
+    path('',VistaPaginaInicio.as_view(), name='inicio'),
+]
